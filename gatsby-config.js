461a4a69c301706c95d284927822b24815e39bd1
Simon de la Rouviere <simon@delarouviere.com>
2019-11-05 15:05:33 +0200

    modify cards

--- gatsby-config.js
+++ gatsby-config.js
@@ -100,7 +100,51 @@ module.exports = {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
-	  'gatsby-remark-social-cards',
+       	  {
+            resolve: `gatsby-remark-social-cards`,
+            options: {
+              title: {
+                // This is the frontmatter field the title should come from
+                field: "title",
+                // Currently only supports DejaVuSansCondensed
+                // More fonts coming soon!
+                font: "DejaVuSansCondensed",
+                color: "black", // black|white
+                size: 48, // 16|24|32|48|64
+                style: "bold", // normal|bold|italic
+                x: null, // Will default to xMargin
+                y: null, // Will default to yMargin
+              },
+              meta: {
+                // The parts array is what generates the bottom text
+                // Pass an array with strings and objects
+                // The following array will generate:
+                // "- Author Name » September 13"
+                // The objects are used to pull data from your markdown's
+                // frontmatter. { field: "author" } pulls the author set
+                // in the frontmatter. { field: "category" } would pull
+                // the category set. Any field can be used as parts
+                // Note: Only pass the "format" property on date fields
+                parts: [
+                  "- ",
+                  { field: "author" },
+                  " » ",
+                  { field: "date", format: "mmmm dS" },
+                ],
+                // Currently only supports DejaVuSansCondensed
+                // More fonts coming soon!
+                font: "DejaVuSansCondensed",
+                color: "white", // black|white
+                size: 24, // 16|24|32|48|64
+                style: "normal", // normal|bold|italic
+                x: null, // Will default to xMargin
+                y: null, // Will default to cardHeight - yMargin - size
+              },
+              background: "#9400D3", // Background color for the card
+              xMargin: 24, // Edge margin used when x value is not set
+              yMargin: 24,// Edge margin used when y value is not set
+            }
+          },
           'gatsby-remark-relative-images',
           {
             resolve: 'gatsby-remark-katex',
